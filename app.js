46f708569d9910cf328004ded6e4f61ccf1f75c5
Jhoan-rios <148820952+Jhoan-rios@users.noreply.github.com>
2023-12-19 12:06:14 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,89 @@
+
+function dark(){
+    document.getElementById('body').classList.remove('ligth')
+    document.getElementById('body').classList.add('dark')
+    localStorage.setItem
+    document.getElementById("logo").innerHTML = `<img src="./imagenes/jhoan/1.png" style="width: 500px;" alt="...">`
+}
+function ligth(){
+    document.getElementById('body').classList.remove('dark') 
+    document.getElementById('body').classList.add('ligth')
+    document.getElementById("logo").innerHTML = `<img src="./imagenes/jhoan/2.png" style="width: 500px;" alt="...">`
+    localStorage.setItem('dark-mode','0')
+}
+
+function español(){
+    document.getElementById('sobre_mi').innerText = "Sobre mi"
+    document.getElementById('proyectos1').innerText = "Proyectos"
+    document.getElementById('contacto1').innerText = "Contacto"
+    document.getElementById('proyectos').innerText = "Proyectos"
+    document.getElementById('contacto').innerText = "Contacto"
+    document.getElementById('contacto2').innerText = "Contacto"
+    document.getElementById('idioma').innerText = "Idioma"
+    document.getElementById('spanish').innerText = "Español"
+    document.getElementById('english').innerText = "Ingles"
+    document.getElementById('tema').innerText = "Tema"
+    document.getElementById('claro').innerText = "Claro"
+    document.getElementById('oscuro').innerText = "Oscuro"
+    document.getElementById('bienvenida').innerText = "Bienvenidos, Yo soy Jhoan"
+    document.getElementById('subtitle').innerText = "Desarrollador de este portafolio"
+    document.getElementById('texto1').innerText = "Soy un programador en proceso que ya cuento con una técnica en desarrollo de software, tengo unas bases de inglés y sigo estudiando para ser mejor cada día. soy muy sociable, muy creativo y cuento con una experiencia en creación de distintos proyectos, puedo adaptarme a cualquier equipo de trabajo y funciono muy bien bajo presión, puedo ser la persona ideal que tu empresa o proyecto necesita. Aquí puedes ver mi portafolio anterior para conocer mi proceso."
+    document.getElementById('link').innerText = "Portafolio anterior"
+    document.getElementById('redes').innerText = "Redes sociales"
+    document.getElementById('habilidades').innerText = "Habilidades"
+    document.getElementById('redes').innerText = "Redes sociales"
+    document.getElementById('sitios').innerText = "Sitios Web"
+    document.getElementById('bases').innerText = "Databases"
+    document.getElementById('lenguajes').innerText = "Lenguajes Backend"
+    document.getElementById('lenguajes2').innerText = "Lenguajes Frontend"
+    document.getElementById('footer1').innerText = "Ponte en contacto conmigo si deseas trabajar conmigo o si deseas conocer mas acerca de mi."
+    document.getElementById('proyect1').innerText = "Este fue mi primer proyecto que desarrolle en Riwi"
+    document.getElementById('proyect2').innerText = "Hoja de vida utilizando html y css"
+    document.getElementById('proyect3').innerText = "Perfil de instagram utilizando html y css"
+    document.getElementById('proyect4').innerText = "Una lista de compras que tiene distintas funciones"
+    document.getElementById('proyect5').innerText = "Proyecto grupal en la que hicimos una pagina de una panaderia"
+    document.getElementById('proyect6').innerText = "Creación de una calculadora utilizando html, css y javascript"
+    document.getElementById('proyect7').innerText = "Creacion de tablas que imprime informacion"
+    document.getElementById('proyect8').innerText = "Creación proyecto Macondo"
+    document.getElementById('proyect9').innerText = "Pagina de kfc replicada que cambia de tema y de idioma"
+    document.getElementById('proyect10').innerText = "Inicio de sesión funcional con Perfil"
+    document.getElementById('proyect11').innerText = "Creacion de proyecto Hogwarts"
+}
+function ingles(){
+    document.getElementById('sobre_mi').innerText = "About me"
+    document.getElementById('proyectos1').innerText = "Projects"
+    document.getElementById('contacto1').innerText = "Contact"
+    document.getElementById('proyectos').innerText = "Projects"
+    document.getElementById('contacto').innerText = "Contact"
+    document.getElementById('contacto2').innerText = "Contact"
+    document.getElementById('idioma').innerText = "Lenguaje"
+    document.getElementById('spanish').innerText = "Spanish"
+    document.getElementById('english').innerText = "English"
+    document.getElementById('tema').innerText = "Theme"
+    document.getElementById('claro').innerText = "Ligth"
+    document.getElementById('oscuro').innerText = "Dark"
+    document.getElementById('bienvenida').innerText = "Welcome, I'm Jhoan"
+    document.getElementById('subtitle').innerText = "Developer of this project"
+    document.getElementById('texto1').innerText = "I am a programmer in process that already have a technique in software development, I have some English basics and I keep studying to be better every day. I am very sociable, very creative and I have experience in creating different projects, I can adapt to any team and I work very well under pressure, I can be the ideal person that your company or project needs. Here you can see my previous portfolio to know my process."
+    document.getElementById('link').innerText = "previous portfolio"
+    document.getElementById('redes').innerText = "Social networks"
+    document.getElementById('habilidades').innerText = "Skills"
+    document.getElementById('redes').innerText = "web sites"
+    document.getElementById('sitios').innerText = "Social networks"
+    document.getElementById('bases').innerText = "Databases"
+    document.getElementById('lenguajes').innerText = "Backend Languages"
+    document.getElementById('lenguajes2').innerText = "Frontend Languages"
+    document.getElementById('footer1').innerText = "Contact me if you want to work with me or if you want to know more about me."
+    document.getElementById('proyect1').innerText = "This was my first project I developed at Riwi."
+    document.getElementById('proyect2').innerText = "Resume using html and css"
+    document.getElementById('proyect3').innerText = "Instagram profile using html and css"
+    document.getElementById('proyect4').innerText = "A shopping list that has different functions"
+    document.getElementById('proyect5').innerText = "Group project in which we made a website of a bakery."
+    document.getElementById('proyect6').innerText = "Creating a calculator using html, css and javascript"
+    document.getElementById('proyect7').innerText = "Creation of tables printing information"
+    document.getElementById('proyect8').innerText = "Creation of Macondo project"
+    document.getElementById('proyect9').innerText = "Replicated kfc page that changes the subject and language"
+    document.getElementById('proyect10').innerText = "Functional login with Profile"
+    document.getElementById('proyect11').innerText = "Creation of Hogwarts project"
+    
+}
